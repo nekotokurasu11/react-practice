1273f80c4dcf850739fe5a57702c200340678fd5
sakasanokuma <moonsunphoenix@gmail.com>
2020-07-14 20:32:50 +0900

    jsx syntax

--- app/src/App.js
+++ app/src/App.js
@@ -1,27 +1,43 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route
-} from "react-router-dom";
-import Tag from './components/Tag.js'
-import TagCss from './components/TagCss.js'
+import React, { Component } from 'react';
+// import {
+//   BrowserRouter as Router,
+//   Switch,
+//   Route
+// } from "react-router-dom";
+// import Tag from './components/Tag.js'
+// import TagCss from './components/TagCss.js'
 
-const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/">
-        <h1>Hello, world!</h1>
-      </Route>
-      <Route exact path="/tag">
-        <Tag />
-      </Route>
-      <Route exact path="/tagcss">
-        <TagCss />
-      </Route>
-    </Switch>
-  </Router>
-);
+class App extends Component {
+  render() {
+    // const greeting = "Hi, Tom!";
+    // const dom = <h1 className="foo">{greeting}</h1>
+    // return dom;
+
+    // return <input type="text" onClick={()=> {console.log("I am clicked")}}/>;
+    return(
+    <React.Fragment>
+      <label htmlFor="bar">bar</label>
+      <input type="text" onChange={() => { console.log("I am clicked") }} />
+    </React.Fragment>
+    )
+  }
+}
+
+// const App = () => (
+//   <Router>
+//     <Switch>
+//       <Route exact path="/">
+//         <h1>Hello, world!</h1>
+//       </Route>
+//       <Route exact path="/tag">
+//         <Tag />
+//       </Route>
+//       <Route exact path="/tagcss">
+//         <TagCss />
+//       </Route>
+//     </Switch>
+//   </Router>
+// );
 
 
 // function App() {
