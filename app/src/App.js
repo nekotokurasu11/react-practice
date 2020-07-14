4709282a8d2ec842f7a5685e4b98c7e0e4518cdb
sakasanokuma <moonsunphoenix@gmail.com>
2020-07-14 19:24:46 +0900

    create my own React boilerplate

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import {
   BrowserRouter as Router,
   Switch,
@@ -13,22 +11,7 @@ const App = () => (
   <Router>
     <Switch>
       <Route exact path="/">
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-        </p>
-            <a
-              className="App-link"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn React
-        </a>
-          </header>
-        </div>
+        <div><h1>Hello, world!</h1></div>
       </Route>
       <Route exact path="/tag">
         <Tag />
